f98b259adc8bc5802c87ae735a6a7bd8324f4af2
Kunal Mishra <118912513+kunalmishravitb@users.noreply.github.com>
2025-07-07 19:48:13 +0530

    Time: 64 ms (89.04%), Space: 75.9 MB (34.26%) - LeetHub

---
+++ 1353-maximum-number-of-events-that-can-be-attended/1353-maximum-number-of-events-that-can-be-attended.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public int maxEvents(int[][] events) {
+        Arrays.sort(events, (a, b) -> a[0] - b[0]);
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        int day = 1, i = 0, n = events.length, count = 0;
+        int maxDay = 0;
+        for (int[] e : events) {
+            maxDay = Math.max(maxDay, e[1]);
+        }
+        while (day <= maxDay) {
+            while (i < n && events[i][0] == day) {
+                minHeap.offer(events[i][1]); 
+                i++;
+            }
+            while (!minHeap.isEmpty() && minHeap.peek() < day) {
+                minHeap.poll();
+            }
+            if (!minHeap.isEmpty()) {
+                minHeap.poll();
+                count++;
+            }
+            day++;
+        }
+        return count;
+    }
+}
