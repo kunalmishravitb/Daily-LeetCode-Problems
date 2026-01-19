f5a34f2998602513a24da8c5bf2d275c5f212c79
Kunal Mishra <118912513+kunalmishravitb@users.noreply.github.com>
2026-01-19 23:29:08 +0530

    Time: 8 ms (45.69%) | Memory: 53.7 MB (93.91%) - LeetSync

--- 1413-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.java
+++ 1413-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.java
@@ -1,36 +1,59 @@
 class Solution {
+
+    private boolean isValid(int[][] pref, int k, int limit) {
+        int n = pref.length;
+        int m = pref[0].length;
+
+        for (int i = k - 1; i < n; i++) {
+            for (int j = k - 1; j < m; j++) {
+                int x1 = i - k + 1;
+                int y1 = j - k + 1;
+
+                int sum = pref[i][j];
+                if (x1 > 0) sum -= pref[x1 - 1][j];
+                if (y1 > 0) sum -= pref[i][y1 - 1];
+                if (x1 > 0 && y1 > 0) sum += pref[x1 - 1][y1 - 1];
+
+                if (sum <= limit)
+                    return true;
+            }
+        }
+        return false;
+    }
+
     public int maxSideLength(int[][] mat, int threshold) {
-        int m = mat.length;
-        int n = mat[0].length;
-        
-        int[][] P = new int[m + 1][n + 1];
-        
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                P[i][j] = mat[i-1][j-1] + P[i-1][j] + P[i][j-1] - P[i-1][j-1];
+        int n = mat.length;
+        int m = mat[0].length;
+
+        int[][] pref = new int[n][m];
+
+        // Copy matrix
+        for (int i = 0; i < n; i++)
+            System.arraycopy(mat[i], 0, pref[i], 0, m);
+
+        // Row-wise prefix sum
+        for (int i = 0; i < n; i++)
+            for (int j = 1; j < m; j++)
+                pref[i][j] += pref[i][j - 1];
+
+        // Column-wise prefix sum
+        for (int j = 0; j < m; j++)
+            for (int i = 1; i < n; i++)
+                pref[i][j] += pref[i - 1][j];
+
+        int low = 1, high = Math.min(n, m);
+        int ans = 0;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (isValid(pref, mid, threshold)) {
+                ans = mid;
+                low = mid + 1;
+            } else {
+                high = mid - 1;
             }
         }
-        
-        int maxSide = 0;
-        
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                int len = maxSide + 1;
-                
-                if (i >= len && j >= len) {
-                    int r1 = i - len + 1;
-                    int c1 = j - len + 1;
-                    
-                    // Calculate sum of square defined by (r1, c1) and (i, j)
-                    int total = P[i][j] - P[r1-1][j] - P[i][c1-1] + P[r1-1][c1-1];
-                    
-                    if (total <= threshold) {
-                        maxSide++;
-                    }
-                }
-            }
-        }
-        
-        return maxSide;
+
+        return ans;
     }
 }
