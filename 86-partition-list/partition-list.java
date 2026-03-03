a5ad4c5c781f0bdcda4d996b957d72d3a91fd275
Kunal Mishra <118912513+kunalmishravitb@users.noreply.github.com>
2026-03-03 21:40:34 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (69.32%) - LeetSync

---
+++ 86-partition-list/partition-list.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode partition(ListNode head, int x) {
+        // Two dummy nodes to start the two lists
+        ListNode lessDummy = new ListNode(0);
+        ListNode greaterDummy = new ListNode(0);
+        
+        // Pointers to the current end of the two lists
+        ListNode less = lessDummy;
+        ListNode greater = greaterDummy;
+        
+        while (head != null) {
+            if (head.val < x) {
+                less.next = head;
+                less = less.next;
+            } else {
+                greater.next = head;
+                greater = greater.next;
+            }
+            head = head.next;
+        }
+        
+        // Important: end the greater list to avoid cycles
+        greater.next = null;
+        
+        // Connect the "less" list to the "greater" list
+        less.next = greaterDummy.next;
+        
+        return lessDummy.next;
+    }
+}
